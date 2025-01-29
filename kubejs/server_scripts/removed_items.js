e3e0f6b2c327687fa214cebe22767d73ca6ac77e
l0py2 <l0py2.git@gmail.com>
2025-01-29 22:48:30 +0000

    Added Almost Unified

--- kubejs/server_scripts/removed_items.js
+++ kubejs/server_scripts/removed_items.js
@@ -29,14 +29,7 @@ ServerEvents.recipes(event => {
 		replaceOutput(original, replacement);
 	}
 
-	replaceAll(global.id.CA('electrum_sheet'), global.tag.M('plates/electrum'));
-	replaceAll(global.id.CR('copper_sheet'), global.tag.M('plates/copper'));
-	replaceAll(global.id.CR('golden_sheet'), global.tag.M('plates/gold'));
-	replaceAll(global.id.CR('iron_sheet'), global.tag.M('plates/iron'));
 	replaceAll(global.id.FD('rope'), global.tag.M('rope'));
-	replaceAll(global.id.CA('iron_rod'), global.tag.M('plates/iron'));
-	replaceInput(global.id.CN('copper_wire'), global.tag.M('wires/copper'));
-	replaceInput(global.id.CA('electrum_wire'), global.tag.M('wires/electrum'));
 
 	global.removedItems.forEach(item => {
 		event.remove({
