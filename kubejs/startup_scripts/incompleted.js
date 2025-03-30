9e9ee3ffece624910d398e130cc4cdf6794ed334
l0py2 <l0py2.git@gmail.com>
2025-03-30 21:38:20 +0100

    Added various items

--- kubejs/startup_scripts/incompleted.js
+++ kubejs/startup_scripts/incompleted.js
@@ -1,3 +1,44 @@
 StartupEvents.registry('item', event => {
+	event.create('incomplete_deagle').texture('kubejs:item/gun/deagle').maxStackSize(1);
+	event.create('incomplete_cz75').texture('kubejs:item/gun/cz75').maxStackSize(1);
+	event.create('incomplete_glock_17').texture('kubejs:item/gun/glock_17').maxStackSize(1);
+	event.create('incomplete_p320').texture('kubejs:item/gun/p320').maxStackSize(1);
+	event.create('incomplete_deagle_golden').texture('kubejs:item/gun/deagle_golden').maxStackSize(1);
 	event.create('incomplete_m1911').texture('kubejs:item/gun/m1911').maxStackSize(1);
+
+	event.create('incomplete_m700').texture('kubejs:item/gun/m700').maxStackSize(1);
+	event.create('incomplete_m107').texture('kubejs:item/gun/m107').maxStackSize(1);
+	event.create('incomplete_m95').texture('kubejs:item/gun/m95').maxStackSize(1);
+	event.create('incomplete_ai_awp').texture('kubejs:item/gun/ai_awp').maxStackSize(1);
+
+	event.create('incomplete_sks_tactical').texture('kubejs:item/gun/sks_tactical').maxStackSize(1);
+	event.create('incomplete_type_81').texture('kubejs:item/gun/type_81').maxStackSize(1);
+	event.create('incomplete_qbz_95').texture('kubejs:item/gun/qbz_95').maxStackSize(1);
+	event.create('incomplete_ak47').texture('kubejs:item/gun/ak47').maxStackSize(1);
+	event.create('incomplete_hk416d').texture('kubejs:item/gun/hk416d').maxStackSize(1);
+	event.create('incomplete_m4a1').texture('kubejs:item/gun/m4a1').maxStackSize(1);
+	event.create('incomplete_m16a1').texture('kubejs:item/gun/m16a1').maxStackSize(1);
+	event.create('incomplete_hk_g3').texture('kubejs:item/gun/hk_g3').maxStackSize(1);
+	event.create('incomplete_m16a4').texture('kubejs:item/gun/m16a4').maxStackSize(1);
+	event.create('incomplete_mk14').texture('kubejs:item/gun/mk14').maxStackSize(1);
+	event.create('incomplete_scar_l').texture('kubejs:item/gun/scar_l').maxStackSize(1);
+	event.create('incomplete_scar_h').texture('kubejs:item/gun/scar_h').maxStackSize(1);
+	event.create('incomplete_aug').texture('kubejs:item/gun/aug').maxStackSize(1);
+
+	event.create('incomplete_db_short').texture('kubejs:item/gun/db_short').maxStackSize(1);
+	event.create('incomplete_db_long').texture('kubejs:item/gun/db_long').maxStackSize(1);
+	event.create('incomplete_m870').texture('kubejs:item/gun/m870').maxStackSize(1);
+	event.create('incomplete_aa12').texture('kubejs:item/gun/aa12').maxStackSize(1);
+
+	event.create('incomplete_ump45').texture('kubejs:item/gun/ump45').maxStackSize(1);
+	event.create('incomplete_uzi').texture('kubejs:item/gun/uzi').maxStackSize(1);
+	event.create('incomplete_hk_mp5a5').texture('kubejs:item/gun/hk_mp5a5').maxStackSize(1);
+	event.create('incomplete_vector45').texture('kubejs:item/gun/vector45').maxStackSize(1);
+	event.create('incomplete_p90').texture('kubejs:item/gun/p90').maxStackSize(1);
+
+	event.create('incomplete_m320').texture('kubejs:item/gun/m320').maxStackSize(1);
+	event.create('incomplete_rpg7').texture('kubejs:item/gun/rpg7').maxStackSize(1);
+
+	event.create('incomplete_m249').texture('kubejs:item/gun/m249').maxStackSize(1);
+	event.create('incomplete_rpk').texture('kubejs:item/gun/rpk').maxStackSize(1);
 });
