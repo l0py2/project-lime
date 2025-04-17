7265818310f8e0f139d7941d911c3e661f6407d5
l0py2 <l0py2.git@gmail.com>
2025-04-17 15:09:24 +0100

    Added recipes for various pistols and various ammunitions

--- kubejs/server_scripts/guns/ammo.js
+++ kubejs/server_scripts/guns/ammo.js
@@ -1,3 +1,71 @@
 ServerEvents.recipes(event => {
+	event.stonecutting(global.id.KJ('incomplete_9mm'), global.tag.M('gunpowder'));
 	event.stonecutting(global.id.KJ('incomplete_45acp'), global.tag.M('gunpowder'));
+	event.stonecutting(global.id.KJ('incomplete_50ae'), global.tag.M('gunpowder'));
+	event.stonecutting(global.id.KJ('incomplete_357'), global.tag.M('gunpowder'));
+
+	let incomplete;
+
+	incomplete = global.id.KJ('incomplete_9mm');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('ammo'), '{AmmoId:"tacz:9mm"}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/brass')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/copper')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(1);
+
+	incomplete = global.id.KJ('incomplete_45acp');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('ammo'), '{AmmoId:"tacz:45acp"}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/brass')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/copper')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(1);
+
+	incomplete = global.id.KJ('incomplete_50ae');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('ammo'), '{AmmoId:"tacz:50ae"}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/brass')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/copper')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(1);
+
+	incomplete = global.id.KJ('incomplete_357');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('ammo'), '{AmmoId:"tacz:357mag"}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/brass')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('gunpowder')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('nuggets/copper')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(1);
 });
--- kubejs/server_scripts/guns/pistols.js
+++ kubejs/server_scripts/guns/pistols.js
@@ -1,7 +1,55 @@
 ServerEvents.recipes(event => {
+	event.stonecutting(global.id.KJ('incomplete_deagle_golden'), global.tag.M('plates/iron'));
 	event.stonecutting(global.id.KJ('incomplete_m1911'), global.tag.M('plates/iron'));
 
-	let incomplete = global.id.KJ('incomplete_m1911');
+	let incomplete;
+
+	incomplete = global.id.KJ('incomplete_deagle_golden');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:deagle_golden",HasBulletInBarrel:0b}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/gold')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('precision_mechanism')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(3);
+
+	incomplete = global.id.KJ('incomplete_p320');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:p320",HasBulletInBarrel:0b}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
+			event.recipes.createFilling(incomplete, [incomplete, Fluid.of(global.id.KJ('liquid_plastic'), FluidAmounts.BOTTLE)]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(3);
+
+	incomplete = global.id.KJ('incomplete_glock_17');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:glock_17",HasBulletInBarrel:0b}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
+			event.recipes.createFilling(incomplete, [incomplete, Fluid.of(global.id.KJ('liquid_plastic'), FluidAmounts.BOTTLE)]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(3);
+
+	incomplete = global.id.KJ('incomplete_m1911');
 	event.recipes.create.sequenced_assembly(
 		[
 			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:m1911",HasBulletInBarrel:0b}')
@@ -14,5 +62,35 @@ ServerEvents.recipes(event => {
 			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
 			event.recipes.createPressing(incomplete, incomplete)
 		]
-	).transitionalItem(incomplete).loops(5);
+	).transitionalItem(incomplete).loops(3);
+
+	incomplete = global.id.KJ('incomplete_cz75');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"AUTO",GunId:"tacz:cz75",HasBulletInBarrel:0b}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
+			event.recipes.createFilling(incomplete, [incomplete, Fluid.of(global.id.KJ('liquid_plastic'), FluidAmounts.BOTTLE)]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(3);
+
+	incomplete = global.id.KJ('incomplete_deagle');
+	event.recipes.create.sequenced_assembly(
+		[
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:deagle",HasBulletInBarrel:0b}')
+		],
+		incomplete,
+		[
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
+			event.recipes.createFilling(incomplete, [incomplete, Fluid.of(global.id.KJ('liquid_plastic'), FluidAmounts.BOTTLE)]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('precision_mechanism')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createPressing(incomplete, incomplete)
+		]
+	).transitionalItem(incomplete).loops(3);
 });
