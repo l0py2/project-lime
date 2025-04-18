1521585573a41894e31d30baec766c2931fc78cf
l0py2 <l0py2.git@gmail.com>
2025-04-18 11:54:24 +0100

    Added recipes for explosive guns and ammunitions

--- kubejs/server_scripts/guns/explosive.js
+++ kubejs/server_scripts/guns/explosive.js
@@ -1,48 +1,42 @@
 ServerEvents.recipes(event => {
-	event.stonecutting(global.id.KJ('incomplete_rpk'), global.tag.M('plates/iron'));
-	event.stonecutting(global.id.KJ('incomplete_m249'), global.tag.M('plates/iron'));
+	event.stonecutting(global.id.KJ('incomplete_m320'), global.tag.M('plates/iron'));
+	event.stonecutting(global.id.KJ('incomplete_rpg7'), global.tag.M('plates/iron'));
 
 	let incomplete;
 
-	incomplete = global.id.KJ('incomplete_rpk');
+	incomplete = global.id.KJ('incomplete_m320');
 	event.recipes.create.sequenced_assembly(
 		[
-			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:rpk",HasBulletInBarrel:0b}')
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:m320",HasBulletInBarrel:1b}')
 		],
 		incomplete,
 		[
 			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.tag.MC('logs')]),
 			event.recipes.createFilling(incomplete, [incomplete, Fluid.of(global.id.KJ('liquid_plastic'), FluidAmounts.BOTTLE)]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
 			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('precision_mechanism')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('precision_mechanism')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
 			event.recipes.createPressing(incomplete, incomplete),
 			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
 			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('rods/iron')]),
 			event.recipes.createPressing(incomplete, incomplete)
 		]
 	).transitionalItem(incomplete).loops(5);
 
-	incomplete = global.id.KJ('incomplete_m249');
+	incomplete = global.id.KJ('incomplete_rpg7');
 	event.recipes.create.sequenced_assembly(
 		[
-			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"AUTO",GunId:"tacz:m249",HasBulletInBarrel:1b}')
+			Item.of(global.id.TZ('modern_kinetic_gun'), '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"tacz:rpg7",HasBulletInBarrel:1b}')
 		],
 		incomplete,
 		[
 			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
 			event.recipes.createFilling(incomplete, [incomplete, Fluid.of(global.id.KJ('liquid_plastic'), FluidAmounts.BOTTLE)]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
 			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('precision_mechanism')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('precision_mechanism')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.id.CR('cogwheel')]),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
 			event.recipes.createPressing(incomplete, incomplete),
+			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
 			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/iron')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
-			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('plates/obsidian')]),
 			event.recipes.createDeploying(incomplete, [incomplete, global.tag.M('rods/iron')]),
 			event.recipes.createPressing(incomplete, incomplete)
 		]
