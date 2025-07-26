5ca07de377da3d910f43ee25e698de533d027590
l0py2 <l0py2.git@gmail.com>
2025-07-26 16:15:14 +0100

    Added various materials

---
+++ kubejs/startup_scripts/new-items/materials.js
@@ -0,0 +1,8 @@
+StartupEvents.registry('item', event => {
+	event.create('compressed_iron_plate').texture('kubejs:item/plate/compressed_iron_plate');
+
+	event.create('iron_rod').texture('kubejs:item/rod/compressed_iron_rod');
+
+	event.create('iron_gear').texture('kubejs:item/gear/iron_gear');
+	event.create('steel_gear').texture('kubejs:item/gear/steel_gear');
+});
