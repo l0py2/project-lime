8bba18bf610284923c96089e49f80993e0a9530a
l0py2 <l0py2.git@gmail.com>
2025-07-30 21:25:45 +0100

    Updated various textures and mods

--- kubejs/startup_scripts/new-items/materials.js
+++ kubejs/startup_scripts/new-items/materials.js
@@ -2,6 +2,9 @@ StartupEvents.registry('item', event => {
 	event.create('compressed_iron_plate').texture('kubejs:item/plate/compressed_iron_plate');
 
 	event.create('compressed_iron_rod').texture('kubejs:item/rod/compressed_iron_rod');
+	event.create('calorite_rod').texture('kubejs:item/rod/calorite_rod');
+	event.create('desh_rod').texture('kubejs:item/rod/desh_rod');
+	event.create('ostrum_rod').texture('kubejs:item/rod/ostrum_rod');
 
 	event.create('iron_gear').texture('kubejs:item/gear/iron_gear');
 	event.create('steel_gear').texture('kubejs:item/gear/steel_gear');
