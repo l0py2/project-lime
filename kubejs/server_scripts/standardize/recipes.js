5976e0ef3172f001f32cfe14525d0cb1c7d81c1f
l0py2 <l0py2.git@gmail.com>
2026-04-02 18:12:27 +0100

    Improved removed items recipes removal

--- kubejs/server_scripts/standardize/recipes.js
+++ kubejs/server_scripts/standardize/recipes.js
@@ -19,15 +19,70 @@ ServerEvents.recipes(event => {
 		);
 	}
 	
+	const recipes = global.recipes.getAll(event, {}, true);
+	const resultProperties = [
+		'result',
+		'results',
+		'output'
+	];
+	
+	const removedRecipes = new Set();
+	const modifiedRecipes = new Map();
+	
+	for(const recipe of recipes) {
+		for(const item of global.removedItems) {
+			for(const resultProperty of resultProperties) {
+				if(recipe[resultProperty] != undefined) {
+					if(typeof recipe[resultProperty] == 'string' && item == recipe[resultProperty]) {
+						removedRecipes.add(recipe.id);
+					} else if(Array.isArray(recipe[resultProperty])) {
+						let removedItemResultIndex;
+						
+						do {
+							removedItemResultIndex = -1;
+						
+							for(let i = 0; i < recipe[resultProperty].length; i++) {
+								if(typeof recipe[resultProperty][i] == 'string' && item == recipe[resultProperty][i]) {
+									removedItemResultIndex = i;
+									break;
+								} else if(recipe[resultProperty][i].item != undefined && item == recipe[resultProperty][i].item) {
+									removedItemResultIndex = i;
+									break;
+								}
+							}
+							
+							if(removedItemResultIndex != -1) {
+								recipe[resultProperty].splice(removedItemResultIndex, 1);
+								modifiedRecipes.set(recipe.id, recipe);
+							}
+							
+							if(recipe[resultProperty].length == 0) {
+								removedRecipes.add(recipe.id);
+							}
+						} while(removedItemResultIndex != -1);
+					} else if(recipe[resultProperty].item != undefined && item == recipe[resultProperty].item) {
+						removedRecipes.add(recipe.id);
+					}
+				}
+			}
+		}
+	}
+
+	for(const recipe of removedRecipes) {
+		modifiedRecipes.delete(recipe);
+		event.remove({ id: recipe });
+	}
+	
+	for(const [recipeId, recipe] of modifiedRecipes) {
+		event.remove({ id: recipeId });
+		event.custom(recipe).id(recipeId);
+	}
+	
 	event.replaceInput(
 		{ input: global.tag.KJ('removed') },
 		global.tag.KJ('removed'),
-		global.id.MC('air')
+		global.id.MC('barrier')
 	);
 	
-	event.replaceOutput(
-		{ output: global.tag.KJ('removed') },
-		global.tag.KJ('removed'),
-		global.id.MC('air')
-	);
+	event.remove({ output: global.tag.KJ('removed') });
 });
