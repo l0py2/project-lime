b8d94d934da022c2f1e7e80592d74d5a7f3e977d
l0py2 <l0py2.git@gmail.com>
2026-04-05 17:02:42 +0100

    Added a script to create symlinks between an instance and the repository

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+help_info() {
+	printf 'reset-server - to remove server configuration files from all the saves\n'
+}
+
 if [ -z "$1" ]
 then
 	printf 'Mode not specified\n\n'
-	printf 'pull - to copy files from instance to repository\n'
-	printf 'push - to copy files from repository to instance\n'
+	help_info
 	exit 1
 fi
 
@@ -15,37 +18,13 @@ else
 	MINECRAFT_DIR="$HOME/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Project Lime/minecraft"
 fi
 
-KUBEJS_DIR="$MINECRAFT_DIR/kubejs"
-CONFIG_DIR="$MINECRAFT_DIR/config"
-DEFAULT_SERVER_CONFIG_DIR="$MINECRAFT_DIR/defaultconfigs"
-
 cd ..
 
-if [ "$1" = 'pull' ]
-then
-	rm -rf ./kubejs
-	cp -rfT "$KUBEJS_DIR" ./kubejs
-	rm -rf ./config
-	cp -rfT "$CONFIG_DIR" ./config
-	rm -rf ./defaultconfigs
-	cp -rfT "$DEFAULT_SERVER_CONFIG_DIR" ./defaultconfigs
-	
-	packwiz refresh
-elif [ "$1" = 'push' ]
-then
-	rm -rf "$KUBEJS_DIR"
-	cp -rfT ./kubejs "$KUBEJS_DIR"
-	rm -rf "$CONFIG_DIR"
-	cp -rfT ./config "$CONFIG_DIR"
-	rm -rf "$DEFAULT_SERVER_CONFIG_DIR"
-	cp -rfT ./defaultconfigs "$DEFAULT_SERVER_CONFIG_DIR"
-elif [ "$1" = 'reset-server' ]
+if [ "$1" = 'reset-server' ]
 then
 	find "$MINECRAFT_DIR/saves" -mindepth 1 -maxdepth 1 -type d -exec rm -rf "{}/serverconfig" \;
 else
 	printf 'Invalid mode specified\n\n'
-	printf 'pull - to copy files from instance to repository\n'
-	printf 'push - to copy files from repository to instance\n'
-	printf 'reset-server - to remove server configuration files from all the saves\n'
+	help_info
 	exit 1
 fi
