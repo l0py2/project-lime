f33058ee64e0710fa8dcaa6104260c96d0444e87
l0py2 <l0py2.git@gmail.com>
2026-04-09 17:08:32 +0100

    Improved readability of the standardization server script

--- kubejs/server_scripts/standardize/recipes.js
+++ kubejs/server_scripts/standardize/recipes.js
@@ -1,266 +1,246 @@
-ServerEvents.recipes(event => {	
-	function equalIngredients(ingredient1, ingredient2) {
-		if(ingredient1 == ingredient2) {
-			return true;
-		}
-		
-		if(typeof ingredient1 == 'string') {
-			ingredient1 = ingredient1.startsWith('#') ? { tag: ingredient1.slice(1) } : { item: ingredient1 };
-		}
-		
-		if(typeof ingredient2 == 'string') {
-			ingredient2 = ingredient2.startsWith('#') ? { tag: ingredient2.slice(1) } : { item: ingredient2 };
-		}
-		
-		return ingredient1.item == ingredient2.item && ingredient1.tag == ingredient2.tag;
+function ingredientStringToObject(ingredientString) {
+	return ingredientString.startsWith('#') ? { tag: ingredientString.slice(1) } : { item: ingredientString };
+}
+
+function equalIngredients(ingredient1, ingredient2) {
+	return ingredient1.item == ingredient2.item
+		&& ingredient1.tag == ingredient2.tag;
+}
+
+function replaceIngredient(original, replacement) {
+	if(replacement.item != undefined) {
+		original.item = replacement.item;
+		delete original.tag;
+	} else {
+		original.tag = replacement.tag;
+		delete original.item;
 	}
-	
-	function replaceIngredient(ingredient, replacement) {
-		const replacementIsTag = replacement.startsWith('#');
+}
+
+SimpleRecipe.fromJson = (rawRecipe) => {
+	if(rawRecipe.ingredient == undefined) {
+		return null;
+	}
 		
-		if(typeof ingredient == 'string') {
-			if(replacementIsTag) {
-				ingredient = {
-					tag: replacement.slice(1)
-				};
-			} else {
-				ingredient = {
-					item: replacement
-				};
+	if(rawRecipe.result == undefined) {
+		return null;
+	}
+			
+	if(typeof rawRecipe.result != 'string') {
+		return null;
+	}
+		
+	let recipe = new SimpleRecipe();
+	
+	recipe.json = rawRecipe;
+	
+	if(Array.isArray(recipe.json.ingredient)) {
+		recipe.multipleInput = true;
+	}
+		
+	return recipe;
+};
+
+function SimpleRecipe() {
+	this.modified = false;
+	this.json = {};
+	this.multipleInput = false;
+	
+	this.replaceInput = (original, replacement) => {
+		const originalObject = ingredientStringToObject(original);
+		const replacementObject = ingredientStringToObject(replacement);
+		
+		if(this.multipleInput) {
+			for(let i = 0; i < this.json.ingredient.length; i++) {
+				if(equalIngredients(this.json.ingredient[i], originalObject)) {
+					replaceIngredient(this.json.ingredient[i], replacementObject);
+					this.modified = true;
+				}
 			}
 		} else {
-			if(replacementIsTag) {
-				delete ingredient.item;
-				ingredient.tag = replacement.slice(1);
+			if(equalIngredients(this.json.ingredient, originalObject)) {
+				replaceIngredient(this.json.ingredient, replacementObject);
+				this.modified = true;
+			}
+		}
+	};
+	
+	this.replaceOutput = (original, replacement) => {
+		if(this.json.result == original) {
+			this.json.resul = replacement;
+			this.modified = true;
+		}
+	};
+}
+
+ShapedRecipe.fromJson = (rawRecipe) => {
+	if(rawRecipe.key == undefined) {
+		return null;
+	}
+	
+	if(typeof rawRecipe.key != 'object') {
+		return null;
+	}
+	
+	if(rawRecipe.result == undefined) {
+		return null;
+	}
+	
+	if(typeof rawRecipe.result != 'object') {
+		return null;
+	}
+	
+	let recipe = new ShapedRecipe();
+	
+	recipe.json = rawRecipe;
+	
+	for(const key in recipe.json.key) {
+		if(Array.isArray(recipe.json.key[key])) {
+			recipe.multipleInputByKey.set(key, true);
+		} else {
+			recipe.multipleInputByKey.set(key, false);
+		}
+	}
+	
+	return recipe;
+}
+
+function ShapedRecipe() {
+	this.modified = false;
+	this.json = {};
+	this.multipleInputByKey = new Map();
+	
+	this.replaceInput = (original, replacement) => {
+		const originalObject = ingredientStringToObject(original);
+		const replacementObject = ingredientStringToObject(replacement);
+				
+		for(const [key, multipleInput] of this.multipleInputByKey) {
+			if(multipleInput) {
+				for(let i = 0; i < this.json.key[key].length; i++) {
+					if(equalIngredients(this.json.key[key][i], originalObject)) {
+						replaceIngredient(this.json.key[key][i], replacementObject);
+						this.modified = true;
+					}
+				}
 			} else {
-				ingredient.item = replacement;
-			}
-		}
-	}
-	
-	function replaceInputSequencedAssembly(recipe, original, replacement) {
-		if(recipe.sequence == undefined) {
-			return false;
-		}
-		
-		if(!Array.isArray(recipe.sequence)) {
-			return false;
-		}
-		
-		let modified = false;
-		
-		for(const recipeStep of recipe.sequence) {
-			if(!Array.isArray(recipeStep.ingredients)) {
-				continue;
-			}
-			
-			for(const ingredient of recipeStep.ingredients) {
-				if(equalIngredients(original, ingredient)) {
-					replaceIngredient(ingredient, replacement);
-					modified = true;
+				if(equalIngredients(this.json.key[key], originalObject)) {
+					replaceIngredient(this.json.key[key], replacementObject);
+					this.modified = true;
 				}
 			}
 		}
+	};
+	
+	this.replaceOutput = (original, replacement) => {
+		const originalObject = ingredientStringToObject(original);
+		const replacementObject = ingredientStringToObject(replacement);
 		
-		return modified;
+		if(equalIngredients(this.json.result, originalObject)) {
+			replaceIngredient(this.json.result, replacementObject);
+			this.modified = true;
+		}
+	}
+}
+
+ShapelessRecipe.fromJson = (rawRecipe) => {
+	if(rawRecipe.ingredients == undefined) {
+		return null;
 	}
 	
-	function replaceInputGeneric(recipe, original, replacement) {		
-		if(recipe.ingredient != undefined) {
-			if(equalIngredients(original, recipe.ingredient)) {
-				replaceIngredient(recipe.ingredient, replacement);
-				return true;
-			}
-		}
+	if(!Array.isArray(rawRecipe.ingredients)) {
+		return null;
+	}
 		
-		let modified = false;
-		
-		if(recipe.ingredients != undefined && Array.isArray(recipe.ingredients)) {
-			for(const ingredient of recipe.ingredients) {
-				if(equalIngredients(original, ingredient)) {
-					replaceIngredient(ingredient, replacement);
-					modified = true;
-				}
-			}
-		}
-		
-		return modified;
+	if(rawRecipe.result == undefined) {
+		return null;
 	}
 	
-	function replaceOutputGeneric(recipe, original, replacement) {
-		if(recipe.result != undefined) {
-			if(equalIngredients(original, recipe.result)) {
-				replaceIngredient(recipe.result, replacement);
-				return true;
-			}
-		}
-		
-		let modified = false;
-		
-		if(recipe.results != undefined && Array.isArray(recipe.results)) {
-			for(const ingredient of recipe.results) {
-				if(equalIngredients(original, ingredient)) {
-					replaceIngredient(ingredient, replacement);
-					modified = true;
-				}
-			}
-		}
-		
-		return modified;
+	if(typeof rawRecipe.result != 'object') {
+		return null;
 	}
-	
-	function replaceItemTags(recipes, modifiedRecipes) {
-		const commonProperties = [
-			'result',
-			'results',
-			'output',
-			'outputs',
-			'ingredient',
-			'ingredients',
-			'input',
-			'inputs'
-		];
 		
-		for(const [original, replacement] of global.itemTagReplacements) {
-			for(const recipe of recipes) {
-				for(const property of commonProperties) {
-					if(recipe[property] == undefined) {
-						continue;
-					}
-					
-					if(Array.isArray(recipe[property])) {
-						let tagIndex;
-							
-						do {
-							tagIndex = -1;
-							
-							for(let i = 0; i < recipe[property].length; i++) {
-								if(typeof recipe[property][i] == 'object' && recipe[property][i].hasOwnProperty('tag') && original == recipe[property][i].tag) {
-									tagIndex = i;
-									break;
-								}
-							}
-								
-							if(tagIndex != -1) {
-								recipe[property][tagIndex].tag = replacement;
-								modifiedRecipes.set(recipe.id, recipe);
-							}
-						} while(tagIndex != -1);
-					} else if(typeof recipe[property] == 'object' && recipe[property].hasOwnProperty('tag') && original == recipe[property].tag) {
-						recipe[property].tag = replacement;
-						modifiedRecipes.set(recipe.id, recipe);
+	let recipe = new ShapelessRecipe();
+	
+	recipe.json = rawRecipe;
+		
+	return recipe;
+};
+
+function ShapelessRecipe() {
+	this.modified = false;
+	this.json = {};
+	
+	this.replaceInput = (original, replacement) => {
+		const originalObject = ingredientStringToObject(original);
+		const replacementObject = ingredientStringToObject(replacement);
+				
+		for(let i = 0; i < this.json.ingredients.length; i++) {
+			if(Array.isArray(this.json.ingredients[i])) {
+				for(let j = 0; j < this.json.ingredients[i].length; j++) {
+					if(equalIngredients(this.json.ingredients[i][j], originalObject)) {
+						replaceIngredient(this.json.ingredients[i][j], replacementObject);
+						this.modified = true;
 					}
 				}
-			}
-		}
-	}
-
-	function removeItems(recipes, removedRecipes, modifiedRecipes) {
-		const commonProperties = [
-			'result',
-			'results',
-			'output',
-			'outputs'
-		];
-		
-		for(const recipe of recipes) {
-			for(const item of global.removedItems) {
-				for(const property of commonProperties) {
-					if(recipe[property] == undefined) {
-						continue;
-					}
-					
-					if(typeof recipe[property] == 'string' && item == recipe[property]) {
-						removedRecipes.add(recipe.id);
-					} else if(Array.isArray(recipe[property])) {
-						let removedItemResultIndex;
-							
-						do {
-							removedItemResultIndex = -1;
-							
-							for(let i = 0; i < recipe[property].length; i++) {
-								if(typeof recipe[property][i] == 'string' && item == recipe[property][i]) {
-									removedItemResultIndex = i;
-									break;
-								} else if(typeof recipe[property][i] == 'object' && recipe[property][i].hasOwnProperty('item') && item == recipe[property][i].item) {
-									removedItemResultIndex = i;
-									break;
-								}
-							}
-								
-							if(removedItemResultIndex != -1) {
-								recipe[property].splice(removedItemResultIndex, 1);
-								modifiedRecipes.set(recipe.id, recipe);
-							}
-								
-							if(recipe[property].length == 0) {
-								removedRecipes.add(recipe.id);
-							}
-						} while(removedItemResultIndex != -1);
-					} else if(typeof recipe[property] == 'object' && recipe[property].hasOwnProperty('item') && item == recipe[property].item) {
-						removedRecipes.add(recipe.id);
-					}
+			} else {
+				if(equalIngredients(this.json.ingredients[i], originalObject)) {
+					replaceIngredient(this.json.ingredients[i], replacementObject);
+					this.modified = true;
 				}
 			}
 		}
+	};
+	
+	this.replaceOutput = (original, replacement) => {
+		const originalObject = ingredientStringToObject(original);
+		const replacementObject = ingredientStringToObject(replacement);
+		
+		if(equalIngredients(this.json.result, originalObject)) {
+			replaceIngredient(this.json.result, replacementObject);
+			this.modified = true;
+		}
 	}
+}
 
-	for(const filter of global.removedRecipes) {
-		event.remove(filter);
+ServerEvents.recipes(event => {
+	for(const recipeFilter of global.removedRecipes) {
+		event.remove(recipeFilter);
 	}
 	
-	for(const [original, replacement] of global.inputReplacements) {
-		event.replaceInput(
-			{ input: original },
-			original,
-			replacement
-		);
+	const rawRecipes = global.recipes.getAll(event, {}, true);
+	
+	const recipes = [];
+	
+	for(const rawRecipe of rawRecipes) {
+		let recipe;
+		
+		if((recipe = SimpleRecipe.fromJson(rawRecipe)) != null) {
+			recipes.push(recipe);
+		} else if((recipe = ShapedRecipe.fromJson(rawRecipe)) != null) {
+			recipes.push(recipe);
+		} else if((recipe = ShapelessRecipe.fromJson(rawRecipe)) != null) {
+			recipes.push(recipe);
+		}
 	}
 	
-	for(const [original, replacement] of global.inputReplacements) {
-		event.replaceOutput(
-			{ input: original },
-			original,
-			replacement
-		);
-	}
-
-	const recipes = global.recipes.getAll(event, {}, true);
-	const removedRecipes = new Set();
-	const modifiedRecipes = new Map();
-
 	for(const recipe of recipes) {
 		for(const [original, replacement] of global.inputReplacements) {
-			if(recipe.type == global.id.CR('sequenced_assembly')) {
-				if(replaceInputSequencedAssembly(recipe, original, replacement)) {
-					modifiedRecipes.set(recipe.id, recipe);
-				}
-			} else {
-				if(replaceInputGeneric(recipe, original, replacement)) {
-					modifiedRecipes.set(recipe.id, recipe);
-				}
-			}
+			recipe.replaceInput(original, replacement);
 		}
 		
 		for(const [original, replacement] of global.outputReplacements) {
-			if(replaceOutputGeneric(recipe, original, replacement)) {
-				modifiedRecipes.set(recipe.id, recipe);
-			}
+			recipe.replaceOutput(original, replacement);
+		}
+		
+		for(const [original, replacement] of global.itemTagReplacements) {
+			recipe.replaceInput(original, replacement);
 		}
 	}
 	
-	replaceItemTags(recipes, modifiedRecipes);
-	removeItems(recipes, removedRecipes, modifiedRecipes);
-	
-	for(const recipe of removedRecipes) {
-		modifiedRecipes.delete(recipe);
-		event.remove({ id: recipe });
+	for(const recipe of recipes) {
+		if(recipe.modified) {
+			event.remove({ id: recipe.json.id });
+			event.custom(recipe.json);
+		}
 	}
-		
-	for(const [recipeId, recipe] of modifiedRecipes) {
-		event.remove({ id: recipeId });
-		event.custom(recipe).id(recipeId);
-	}
-	
-	event.remove({ output: global.tag.KJ('removed') });
 });
