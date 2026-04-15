d21969fc4c8d3441b5bd3784cd1e9556f66990d7
l0py2 <l0py2.git@gmail.com>
2026-04-15 23:39:11 +0100

    Moved ingredient utility functions

--- kubejs/server_scripts/global/recipe-types/main.js
+++ kubejs/server_scripts/global/recipe-types/main.js
@@ -1,3 +1,22 @@
 //priority: 99
 
 global.recipes.types = [];
+
+function ingredientStringToObject(ingredientString) {
+	return ingredientString.startsWith('#') ? { tag: ingredientString.slice(1) } : { item: ingredientString };
+}
+
+function equalIngredients(ingredient1, ingredient2) {
+	return ingredient1.item == ingredient2.item
+		&& ingredient1.tag == ingredient2.tag;
+}
+
+function replaceIngredient(original, replacement) {
+	if(replacement.item != undefined) {
+		original.item = replacement.item;
+		delete original.tag;
+	} else {
+		original.tag = replacement.tag;
+		delete original.item;
+	}
+}
--- kubejs/server_scripts/standardize/recipes.js
+++ kubejs/server_scripts/standardize/recipes.js
@@ -1,22 +1,3 @@
-function ingredientStringToObject(ingredientString) {
-	return ingredientString.startsWith('#') ? { tag: ingredientString.slice(1) } : { item: ingredientString };
-}
-
-function equalIngredients(ingredient1, ingredient2) {
-	return ingredient1.item == ingredient2.item
-		&& ingredient1.tag == ingredient2.tag;
-}
-
-function replaceIngredient(original, replacement) {
-	if(replacement.item != undefined) {
-		original.item = replacement.item;
-		delete original.tag;
-	} else {
-		original.tag = replacement.tag;
-		delete original.item;
-	}
-}
-
 ServerEvents.recipes(event => {
 	for(const recipeFilter of global.removedRecipes) {
 		event.remove(recipeFilter);
