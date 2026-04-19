1e1d8905cc36a259775452a30dfd7f7910df94e5
l0py2 <l0py2.git@gmail.com>
2026-04-19 11:18:13 +0100

    Removed the necessity of silk touch to break various blocks

--- kubejs/server_scripts/game/drop-block.js
+++ kubejs/server_scripts/game/drop-block.js
@@ -1,28 +1,63 @@
 LootJS.lootTables(event => {
-	// Removes the need of silk touch for blocks like glass
-	const lootTableIds = event.getLootTableIds(/.*blocks\/.*/)
-		.map(resource => resource.toString());
+	const silkTouchBlocks = [
+		'minecraft:blue_ice',
+		'minecraft:bookshelf',
+		'minecraft:campfire',
+		'minecraft:chiseled_bookshelf',
+		'minecraft:ender_chest',
+		'minecraft:gilded_blackstone',
+		'minecraft:glass',
+		'minecraft:glass_pane',
+		'minecraft:ice',
+		'minecraft:packed_ice',
+		'minecraft:sculk_sensor',
+		'minecraft:calibrated_sculk_sensor',
+		'minecraft:sculk_catalyst',
+		'minecraft:sea_lantern',
+		'minecraft:soul_campfire'
+	];
 	
-	for(const lootTableId of lootTableIds) {
-		let lootTable = event.getLootTable(lootTableId);
+	for(const colour of global.dyeColours) {
+		silkTouchBlocks.push(`minecraft:${colour}_stained_glass`);
+		silkTouchBlocks.push(`minecraft:${colour}_stained_glass_pane`);
+	}
+	
+	for(const coral of global.corals) {
+		silkTouchBlocks.push(`minecraft:${coral}_coral_block`);
+	}
+	
+	for(const block of silkTouchBlocks) {
+		let lootTable = event.getBlockTable(block);
 		
-		if(lootTable.getLootType() == LootType.BLOCK) {
-			let blockName = lootTableId.replace('blocks/', '');
-			let blockExists = Item.exists(blockName);
-			
-			if(blockExists) {
-				let entries = lootTable
-					.firstPool()
-					.getEntries();
-				
-				lootTable
-					.clear()
-					.createPool(pool => {
-						for(const entry of entries) {
-							pool.addEntry(entry);
-						}
-					});
-			}
+		if(lootTable != null) {
+			lootTable
+				.clear()
+				.createPool()
+				.addEntry(LootEntry.of(block));
+		}
+	}
+	
+	const shearableBlocks = [
+		'minecraft:turtle_egg'
+	];
+	
+	for(const coral of global.corals) {
+		shearableBlocks.push(`minecraft:${coral}_coral`);
+		shearableBlocks.push(`minecraft:${coral}_coral_fan`);
+	}
+	
+	for(const block of shearableBlocks) {
+		let lootTable = event.getBlockTable(block);
+		
+		if(lootTable != null) {
+			lootTable
+				.clear()
+				.createPool()
+				.addEntry(
+					LootEntry
+						.of(block)
+						.matchTool(ItemFilter.tag('c:tools/shear'))
+				);
 		}
 	}
 });
---
+++ kubejs/startup_scripts/constants.js
@@ -0,0 +1,28 @@
+//priority: 1
+
+global.dyeColours = [
+	'white',
+	'light_gray',
+	'gray',
+	'black',
+	'brown',
+	'red',
+	'orange',
+	'yellow',
+	'lime',
+	'green',
+	'cyan',
+	'light_blue',
+	'blue',
+	'purple',
+	'magenta',
+	'pink'
+];
+
+global.corals = [
+	'tube',
+	'brain',
+	'bubble',
+	'fire',
+	'horn'
+];
