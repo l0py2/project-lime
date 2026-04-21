e337f0d80542663d133659ae7c0aa8f81d464988
l0py2 <l0py2.git@gmail.com>
2026-04-21 16:50:09 +0100

    Added cutting recipes for blocks added in the 1.21 Minecraft update

--- kubejs/server_scripts/block-cutting/mods/minecraft/copper.js
+++ kubejs/server_scripts/block-cutting/mods/minecraft/copper.js
@@ -6,6 +6,7 @@ BlockCutting.types.add('copper');
 BlockCutting.tags.push(['kubejs:blocks/copper', 'minecraft:copper_block']);
 BlockCutting.tags.push(['kubejs:blocks/copper', 'minecraft:cut_copper']);
 BlockCutting.tags.push(['kubejs:blocks/copper', 'minecraft:copper_grate']);
+BlockCutting.tags.push(['kubejs:blocks/copper', 'minecraft:chiseled_copper']);
 BlockCutting.tags.push(['kubejs:slabs/copper', 'minecraft:cut_copper_slab']);
 BlockCutting.tags.push(['kubejs:stairs/copper', 'minecraft:cut_copper_stairs']);
 
@@ -14,6 +15,7 @@ BlockCutting.types.add('waxed_copper');
 BlockCutting.tags.push(['kubejs:blocks/waxed_copper', 'minecraft:waxed_copper_block']);
 BlockCutting.tags.push(['kubejs:blocks/waxed_copper', 'minecraft:waxed_cut_copper']);
 BlockCutting.tags.push(['kubejs:blocks/waxed_copper', 'minecraft:waxed_copper_grate']);
+BlockCutting.tags.push(['kubejs:blocks/waxed_copper', 'minecraft:waxed_chiseled_copper']);
 BlockCutting.tags.push(['kubejs:slabs/waxed_copper', 'minecraft:waxed_cut_copper_slab']);
 BlockCutting.tags.push(['kubejs:stairs/waxed_copper', 'minecraft:waxed_cut_copper_stairs']);
 
@@ -32,6 +34,7 @@ for(const state of states) {
 	BlockCutting.tags.push([`kubejs:blocks/${state}_copper`, `minecraft:${state}_copper`]);
 	BlockCutting.tags.push([`kubejs:blocks/${state}_copper`, `minecraft:${state}_cut_copper`]);
 	BlockCutting.tags.push([`kubejs:blocks/${state}_copper`, `minecraft:${state}_copper_grate`]);
+	BlockCutting.tags.push([`kubejs:blocks/${state}_copper`, `minecraft:${state}_chiseled_copper`]);
 	BlockCutting.tags.push([`kubejs:slabs/${state}_copper`, `minecraft:${state}_cut_copper_slab`]);
 	BlockCutting.tags.push([`kubejs:stairs/${state}_copper`, `minecraft:${state}_cut_copper_stairs`]);
 }
