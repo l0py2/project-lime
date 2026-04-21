dd2865415868bc5f37536b9be12d33421d6edfc4
Lagosta4K <lagosta4k@gmail.com>
2026-04-21 23:05:29 +0100

    Finalized create wearables recipes

--- kubejs/server_scripts/mods/create/wearables.js
+++ kubejs/server_scripts/mods/create/wearables.js
@@ -58,52 +58,58 @@ ServerEvents.recipes(event => {
 	
 	event.remove({ id: 'create:crafting/appliances/netherite_backtank_from_netherite' });
 	event.remove({ id: 'create:crafting/appliances/netherite_backtank' });
-	event.recipes.create.mechanical_crafting(
-		'create:netherite_backtank',
+	CreateMechanicalCraftingRecipe.custom(
+		event, 
 		[
-			'     ',
-			' CCC ',
-			'BDADB',
-			'BDDDB ',
-			' BBB '
+			'BCB',
+			'BAB',
+			'BDB',
+			' B '
+		], 
+		{
+			A: Ingredient.of('create:copper_backtank'),
+			B: Ingredient.of('#c:plates/obsidian'),
+			C: Ingredient.of('#c:ingots/netherite'),
+			D: Ingredient.of('#c:storage_blocks/steel')
+		},
+		Item.of('create:netherite_backtank')
+	);
+
+	
+	event.remove({ id: 'create:crafting/appliances/netherite_diving_helmet_from_netherite' });
+	event.remove({ id: 'create:crafting/appliances/netherite_diving_helmet' });
+	CreateMechanicalCraftingRecipe.custom(
+		event,
+		[
+			'BCB',
+			'BAB',
+			'DED'
 		],
 		{
-			A: 'create:copper_backtank',
-			B: '#c:plates/obsidian',
-			C: '#c:ingots/netherite',
-			D: '#c:storage_blocks/steel'
-		}
+			A: Ingredient.of('create:copper_diving_helmet'),
+			B: Ingredient.of('#c:plates/obsidian'),
+			C: Ingredient.of('#c:ingots/netherite'),
+			D: Ingredient.of('#c:storage_blocks/steel'),
+			E: Ingredient.of('#c:glass_blocks/tinted')
+		},
+		Item.of('create:netherite_diving_helmet')
 	);
 	
-	event.remove({ id: 'create:crafting/appliances/copper_backtank' });
-	event.shaped(
-		'create:copper_backtank',
+	event.remove({ id: 'create:crafting/appliances/netherite_diving_boots_from_netherite' });
+	event.remove({ id: 'create:crafting/appliances/netherite_diving_boots' });
+	CreateMechanicalCraftingRecipe.custom(
+		event,
 		[
-			'DCD',
+			'BCB',
 			'BAB',
-			' B '
+			'D D'
 		],
 		{
-			A: '#c:storage_blocks/copper',
-			B: '#c:plates/copper',
-			C: 'create:shaft',
-			D: 'create:andesite_alloy'
-		}
-	);
-	
-	event.remove({ id: 'create:crafting/appliances/copper_backtank' });
-	event.shaped(
-		'create:copper_backtank',
-		[
-			'DCD',
-			'BAB',
-			' B '
-		],
-		{
-			A: '#c:storage_blocks/copper',
-			B: '#c:plates/copper',
-			C: 'create:shaft',
-			D: 'create:andesite_alloy'
-		}
+			A: Ingredient.of('create:copper_diving_boots'),
+			B: Ingredient.of('#c:plates/obsidian'),
+			C: Ingredient.of('#c:ingots/netherite'),
+			D: Ingredient.of('#c:storage_blocks/steel')
+		},
+		Item.of('create:netherite_diving_boots')
 	);
 });
